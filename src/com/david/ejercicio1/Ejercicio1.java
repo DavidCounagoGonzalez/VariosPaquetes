b586f4c219fd0b8bd4b0b1bdddae8d35f86d902a
David Counago Gonzalez <david.mouriscados@gmail.com>
2021-10-08 09:52:02 +0200

    Share

--- src/com/david/ejercicio1/Ejercicio1.java
+++ src/com/david/ejercicio1/Ejercicio1.java
@@ -3,6 +3,6 @@ package com.david.ejercicio1;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        System.out.println("Ejercicio1");
+        System.out.println("Share on GitHub");
     }
 }
